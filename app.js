999435e8e4efc5921b170df2b128f5a6e4eea902
Rasyahodhe <rasyahodhe450@gmail.com>
2024-03-03 14:35:39 +0700

    lebih meringkas codingan

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const exprees = require("express"),
+  expresslayouts = require("express-ejs-layouts"),
   app = exprees(),
   morgan = require("morgan"),
   port = 3000;
+
 // Setting Express Untuk menggunakan Ejs
 app.set("view engine", "ejs");
+// Mempermudah menggunakan routing perpindahan halaman
+app.use(expresslayouts);
 
 app.get("/", (req, res) => {
-  res.render("index", { bidang: "Rahim", title: "Halaman Home" });
+  res.render("index", {
+    bidang: "Rahim",
+    title: "Halaman Home",
+    layout: "layouts/main.ejs",
+  });
 });
 app.get("/about", (req, res) => {
-  res.render("about", { bidang: "About", title: "Halaman About" });
+  res.render("about", {
+    bidang: "About",
+    title: "Halaman About",
+    layout: "layouts/main.ejs",
+  });
 });
 app.get("/contact", (req, res) => {
-  res.render("contact", { bidang: "Contact", title: "Halaman Kontak" });
+  res.render("contact", {
+    bidang: "Contact",
+    title: "Halaman Kontak",
+    layout: "layouts/main.ejs",
+  });
 });
 app.use("/", (req, res) => {
   res.status(404).send("Halaman Not Found");
