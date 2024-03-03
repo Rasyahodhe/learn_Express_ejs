91e277f69d6b7f39947b65ba9df5cb291f3292e3
Rasyahodhe <rasyahodhe450@gmail.com>
2024-03-03 07:11:15 +0700

    membuat template layout agar bisa di gunakan di banyak halaman

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const exprees = require("express"),
 // Setting Express Untuk menggunakan Ejs
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index", { bidang: "Rahim", title: "Halaman Home" });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { bidang: "About", title: "Halaman About" });
+});
+app.get("/contact", (req, res) => {
+  res.render("contact", { bidang: "Contact", title: "Halaman Kontak" });
+});
 app.use("/", (req, res) => {
   res.status(404).send("Halaman Not Found");
 });
